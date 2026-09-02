7e9510b4c2575d7905e2cdd61e8be728f307ea1a
RLKsBR <ruanlukasc599@gmail.com>
2026-09-02 17:38:20 -0300

    Display tower ranges across all lanes

--- visual-team-range.js
+++ visual-team-range.js
@@ -2,7 +2,7 @@
 (function(){
 'use strict';
 drawTowerRange=function(s){
-  if(!showTowerRanges||s.lane!==selectedLane)return;
+  if(!showTowerRanges)return;
   const x=s.x,y=laneYAt(s.lane,s.x),r=s.range*PX,tm=teamTheme(s.side);
   ctx.save();ctx.globalAlpha=.10;ctx.fillStyle=tm.primary;ctx.beginPath();ctx.arc(x,y,r,0,Math.PI*2);ctx.fill();
   ctx.globalAlpha=.52;ctx.strokeStyle=tm.primary;ctx.lineWidth=3;ctx.setLineDash(tm.shape==='sharp'?[10,8]:[20,13]);ctx.beginPath();ctx.arc(x,y,r,0,Math.PI*2);ctx.stroke();ctx.setLineDash([]);
